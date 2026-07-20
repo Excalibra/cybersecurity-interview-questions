c3df729d398f10fa8fed5973c840c8482cc4d2c3
𝕰𝖝𝖈𝖆𝖑𝖎𝖇𝖗𝖆 <83846602+Excalibra@users.noreply.github.com>
2026-07-20 18:06:48 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -87,7 +87,7 @@ module.exports = {
           label: "Network Security",
           link: {
             type: "doc",
-            id: "questions/network-security-interview/index"
+            id: "questions/network-security-interview/important-protocol-stack-overview"
           },
           items: [
             "questions/network-security-interview/what-is-sql-injection-attack",
